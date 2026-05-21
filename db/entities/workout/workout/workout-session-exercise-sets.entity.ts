7ad652e0e56820d0d28a2ef258c311edd5eaaa8f
Sappakit <pipitwattana.s@gmail.com>
2026-05-21 16:17:05 +0700

    refactor: store workout exercises config at set level

--- db/entities/workout/workout/workout-session-exercise-sets.entity.ts
+++ db/entities/workout/workout/workout-session-exercise-sets.entity.ts
@@ -1,41 +1,25 @@
-import {
-  Column,
-  Entity,
-  Index,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
-import { WorkoutSessionExercise } from '.';
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
+import { WorkoutExerciseSet, WorkoutSessionExercise } from '.';
+import { WorkoutSetBase } from '../shared/workout-set-base.entity';
 
 @Index(['session_exercise', 'set_number'], { unique: true })
 @Entity({ schema: 'workout', name: 'workout_session_exercise_sets' })
-export class WorkoutSessionExerciseSet {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({ type: 'int' })
-  set_number: number;
-
-  @Column({ type: 'int', nullable: true })
-  reps?: number | null;
-
-  @Column({ type: 'numeric', precision: 6, scale: 2, nullable: true })
-  weight?: number | null;
-
-  @Column({ type: 'numeric', precision: 6, scale: 2, nullable: true })
-  distance?: number | null;
-
-  @Column({ type: 'int', nullable: true, comment: 'seconds' })
-  duration?: number | null;
-
+export class WorkoutSessionExerciseSet extends WorkoutSetBase {
   @Column({ type: 'timestamptz', nullable: true })
   performed_at?: Date | null;
 
   @Column({ type: 'timestamptz', nullable: true })
   completed_at?: Date | null;
 
-  @ManyToOne(() => WorkoutSessionExercise, (se) => se.sets, { nullable: false })
+  @ManyToOne(() => WorkoutSessionExercise, (se) => se.sets, {
+    nullable: false,
+  })
   @JoinColumn({ name: 'session_exercise_id' })
   session_exercise: WorkoutSessionExercise;
+
+  @ManyToOne(() => WorkoutExerciseSet, {
+    nullable: true,
+  })
+  @JoinColumn({ name: 'workout_exercise_set_id' })
+  workout_exercise_set?: WorkoutExerciseSet | null;
 }
